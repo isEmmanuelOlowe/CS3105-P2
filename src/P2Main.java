dd6ab1df9cc702deffb59765349432c3f3b1e8f7
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-11-18 15:12:05 +0000

    progress

--- src/P2Main.java
+++ src/P2Main.java
@@ -122,13 +122,13 @@ public class P2Main {
             //// YOUR CODE HERE        
             Object obj = parser.parse(new FileReader(args[3]));
             JSONObject jsonObject = (JSONObject) obj;
-            hiddenLayers = (int) jsonObject.get("n_hidden_layers");
-            hiddenLayerNodes = (int) jsonObject.get("n_nodes_per_hidden_layer");
+            hiddenLayers = jsonObject.get("n_hidden_layers").intValue();
+            hiddenLayerNodes = jsonObject.get("n_nodes_per_hidden_layer").intValue();
             activationFunction = (String) jsonObject.get("activation_function");
-            learningRate = (double) jsonObject.get("learning_rate");
-            batchSize = (int) jsonObject.get("batchsize");
-            epochs = (int) jsonObject.get("nEpochs");
-            patience = (int) jsonObject.get("patience");
+            learningRate = jsonObject.get("learning_rate").doubleValue();
+            batchSize = jsonObject.get("batchsize").intValue();
+            epochs = jsonObject.get("nEpochs").intValue();
+            patience = jsonObject.get("patience").intValue();
             // build and train an ANN with the given data and parameters
             Layer layer = ann.build(trainset.getInputDims(), trainset.getOutDims(), hiddenLayers, hiddenLayerNodes, activationFunction);
             Loss crossEntropy = new CrossEntropy();
