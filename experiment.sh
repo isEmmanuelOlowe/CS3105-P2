08ccaf10e207d7b2b8ca4fe834c423b9eba431d6
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-11-20 06:20:53 +0000

    more

--- experiment.sh
+++ experiment.sh
@@ -1,2 +1,2 @@
 echo "------------- Running Part 2 Experiments ------------"
-java -cp lib/*:minet:. P2Main data/Part3/train.txt data/Part3/test.txt 123 settings/Part2.json 1
+java -cp lib/*:minet:. P2Main data/Part3/train.txt data/Part3/test.txt 123 settings/Part2.json 1 1
